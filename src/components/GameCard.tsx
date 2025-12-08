a4c8b0f6658e3b6d0535464815951afd78bafa94
Fabio Policeno <fabio.policeno@infor.com>
2025-12-08 17:32:33 -0600

    Add emojis

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Game } from '../hooks/useGames';
-import { Card, CardBody, Heading, HStack, Image, Text } from '@chakra-ui/react';
+import { Card, CardBody, Heading, HStack, Image } from '@chakra-ui/react';
 import PlatformIconList from './PlatformIconList';
 import CriticScore from './CriticScore';
 import getCroppedImageUrl from '../services/image-url';
+import Emoji from './Emoji';
 
 interface Props {
    game: Game;
@@ -22,7 +23,10 @@ const GameCard = ({ game }: Props) => {
                />
                <CriticScore score={game.metacritic} />
             </HStack>
-            <Heading fontSize="2xl">{game.name}</Heading>
+            <Heading fontSize="2xl">
+               {game.name}
+               <Emoji rating={game.rating_top} />
+            </Heading>
          </CardBody>
       </Card>
    );
